20f28f3b87779c0d1238dc609d8afa6d471a3707
adurenator <adurenator@gmail.com>
2013-01-30 13:59:14 +0100

    uploading project

---
+++ IP_g14_lab_app/gen/g14app/ip_g14_lab_app/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package g14app.ip_g14_lab_app;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ IP_g14_lab_app/src/activities/StartScreen.java
@@ -0,0 +1,17 @@
+package activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartScreen extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// class initializations
+		// setContentView(R.layout.THE_LAYOUT);
+	}
+	
+}
