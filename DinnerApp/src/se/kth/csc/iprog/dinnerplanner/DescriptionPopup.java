6e66a408d7d672366e9caa62f0263e04ccf356a2
Adur Saizar <adurenator@gmail.com>
2013-02-01 00:17:42 +0100

    My files. Not implemented yet. Just for repository testing.

---
+++ DinnerApp/src/se/kth/csc/iprog/dinnerplanner/DescriptionPopup.java
@@ -0,0 +1,25 @@
+package se.kth.csc.iprog.dinnerplanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DescriptionPopup extends Activity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.description_popup);
+		initLayout();
+	}
+	
+	/***************************************************************/
+	/** PRIVATE METHODS ********************************************/
+	/***************************************************************/
+	
+	private void initLayout()
+	{
+		
+	}
+	
+}
