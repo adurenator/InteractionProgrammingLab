596905027cc93d5f85967a4f38ad54347ea10c15
Adur Saizar <adurenator@gmail.com>
2013-02-03 20:32:13 +0100

    Everything up to date.

--- DinnerApp/gen/se/kth/csc/iprog/dinnerplanner/R.java
+++ DinnerApp/gen/se/kth/csc/iprog/dinnerplanner/R.java
@@ -10,27 +10,105 @@ package se.kth.csc.iprog.dinnerplanner;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int preparation_list_grey=0x7f040001;
+        public static final int preparation_list_lightgrey=0x7f040002;
+        public static final int preparation_list_white=0x7f040000;
+    }
     public static final class drawable {
-        public static final int bakedbrie=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int icecream=0x7f020002;
-        public static final int meatballs=0x7f020003;
-        public static final int sourdough=0x7f020004;
-        public static final int toast=0x7f020005;
+        public static final int back_with_border=0x7f020000;
+        public static final int bakedbrie=0x7f020001;
+        public static final int crossed=0x7f020002;
+        public static final int guest_selection_image_button=0x7f020003;
+        public static final int guests3=0x7f020004;
+        public static final int guests_icon=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int icecream=0x7f020007;
+        public static final int meatballs=0x7f020008;
+        public static final int sourdough=0x7f020009;
+        public static final int start_screen_image=0x7f02000a;
+        public static final int start_screen_image_button=0x7f02000b;
+        public static final int toast=0x7f02000c;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int activity_guest_selection=0x7f080000;
+        public static final int activity_start_screen=0x7f080009;
+        public static final int back_button=0x7f080015;
+        public static final int button1=0x7f080018;
+        public static final int description_popup_back=0x7f080012;
+        public static final int description_popup_image=0x7f080010;
+        public static final int description_popup_ingredients=0x7f08000f;
+        public static final int description_popup_instructions=0x7f080011;
+        public static final int description_popup_title=0x7f08000e;
+        public static final int dinnerAppDescription=0x7f08000c;
+        public static final int dinnerAppTitle=0x7f08000a;
+        public static final int dish_list=0x7f080014;
+        public static final int dish_list_pattern_title=0x7f080013;
+        public static final int expandable_list=0x7f080008;
+        public static final int footer=0x7f080007;
+        public static final int guestSelectionEnteringNumberField=0x7f080003;
+        public static final int guestSelectionEnteringNumberLabel=0x7f080002;
+        public static final int guestSelectionTitle=0x7f080001;
+        public static final int guests_number=0x7f080017;
+        public static final int header=0x7f080006;
+        public static final int imageButton1=0x7f080004;
+        public static final int list_item_parent=0x7f08001d;
+        public static final int menu_settings=0x7f080021;
+        public static final int next_button=0x7f080016;
+        public static final int preparation_dish_description=0x7f08001c;
+        public static final int preparation_dish_image=0x7f08001a;
+        public static final int preparation_dish_name=0x7f08001b;
+        public static final int row_image=0x7f08001f;
+        public static final int row_radio=0x7f08001e;
+        public static final int row_title=0x7f080020;
+        public static final int start_screen_button=0x7f08000d;
+        public static final int start_screen_image=0x7f08000b;
+        public static final int total_price=0x7f080019;
+        public static final int xx=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_guest_selection=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_preparation=0x7f030002;
+        public static final int activity_start_screen=0x7f030003;
+        public static final int description_popup=0x7f030004;
+        public static final int dish_list_pattern=0x7f030005;
+        public static final int footer=0x7f030006;
+        public static final int header=0x7f030007;
+        public static final int list_item_child=0x7f030008;
+        public static final int list_item_group=0x7f030009;
+        public static final int list_item_group_empty=0x7f03000a;
+        public static final int row=0x7f03000b;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int adur_back=0x7f05000c;
+        public static final int adur_guests_img_description=0x7f050015;
+        public static final int adur_ingredients=0x7f050017;
+        public static final int adur_inital_prize=0x7f05000f;
+        public static final int adur_initial_no_guests=0x7f05000e;
+        public static final int adur_initial_title=0x7f050013;
+        public static final int adur_initial_title2=0x7f050014;
+        public static final int adur_next=0x7f05000d;
+        public static final int adur_static_option1=0x7f050010;
+        public static final int adur_static_option2=0x7f050011;
+        public static final int adur_static_option3=0x7f050012;
+        public static final int adur_summary_img_description=0x7f050016;
+        public static final int app_name=0x7f050000;
+        public static final int guest_selection_button_text=0x7f05000a;
+        public static final int guest_selection_entering_number=0x7f050009;
+        public static final int guest_selection_image_button_description=0x7f05000b;
+        public static final int guest_selection_title=0x7f050008;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int prepare_instr=0x7f050018;
+        public static final int start_screen_button_text=0x7f050005;
+        public static final int start_screen_description=0x7f050004;
+        public static final int start_screen_image_button_description=0x7f050007;
+        public static final int start_screen_image_description=0x7f050006;
+        public static final int start_screen_tittle=0x7f050003;
     }
     public static final class style {
         /** 
@@ -54,10 +132,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
