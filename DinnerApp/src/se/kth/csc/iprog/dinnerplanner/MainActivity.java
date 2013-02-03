84deef104b3c5cdf251eee8d7ac5c6141fce91ad
Adur Saizar <adurenator@gmail.com>
2013-02-03 20:08:15 +0100

    Alvaro's part!

--- DinnerApp/src/se/kth/csc/iprog/dinnerplanner/MainActivity.java
+++ DinnerApp/src/se/kth/csc/iprog/dinnerplanner/MainActivity.java
@@ -4,16 +4,38 @@ import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.ViewGroup;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_start_screen);
 		
-		//To get the dinner model you can use the following code:
+		// CUSTOMiZE FONT (We'll use it on the new lab)
+		// Creation of custom fonts.
+		//android.graphics.Typeface tfBalqBold = android.graphics.Typeface.createFromAsset(getAssets(),
+        //                                       "fonts/BalsamiqSansBold.ttf");
+		//android.graphics.Typeface tfBalqBoldIt = android.graphics.Typeface.createFromAsset(getAssets(),
+        //                                       "fonts/BalsamiqSansBoldItalic.ttf");
+		//android.graphics.Typeface tfBalqIt = android.graphics.Typeface.createFromAsset(getAssets(),
+        //                                     "fonts/BalsamiqSansItalic.ttf");
+		//android.graphics.Typeface tfBalqReg = android.graphics.Typeface.createFromAsset(getAssets(),
+        //                                      "fonts/BalsamiqSansRegular.ttf");
+		
+		// Setting of custom fonts on the different textViews
+		//ViewGroup view = (ViewGroup)getWindow().getDecorView();
+		//if(((ViewGroup)findViewById(android.R.id.content)).getChildAt(0).getId()==R.layout.activity_start_screen){
+		//	android.widget.TextView tvStartScreenTitle = (android.widget.TextView) findViewById(R.id.dinnerAppTitle);
+		//	tvStartScreenTitle.setTypeface(tfBalqBoldIt);
+        
+		//	android.widget.TextView tvStartScreenDescription = (android.widget.TextView) findViewById(R.id.dinnerAppDescription);
+		//	tvStartScreenDescription.setTypeface(tfBalqReg);
+		
+		// To get the dinner model you can use the following code:
 		DinnerModel model = ((DinnerPlannerApplication) this.getApplication()).getModel();
 		
 	}
