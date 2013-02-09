40ee10bab9210f4ceb09a3de7cf9c32eb6e01cc6
AlvaroPieor <alvaropeiro@yahoo.es>
2013-02-09 20:50:50 +0100

    Added interaction with the model in GuestSelection

--- DinnerApp/src/controllers/GuestSelection_Controller.java
+++ DinnerApp/src/controllers/GuestSelection_Controller.java
@@ -1,23 +1,35 @@
 package controllers;
 
-import views.GuestSelection_View;
 import android.view.View;
 import android.view.View.OnClickListener;
+
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+import views.GuestSelection_View;
 
 public class GuestSelection_Controller implements OnClickListener{
 	
 	// Activity used by the view
+	private DinnerModel model;
 	private GuestSelection_View view;
-	
 
-	public GuestSelection_Controller(GuestSelection_View associatedView){
+	public GuestSelection_Controller(GuestSelection_View associatedView, DinnerModel associatedModel){
+		model = associatedModel;
+		
 		view = associatedView;
 		view.guestSelectionConfirmButton.setOnClickListener(this);
+		
 	}
 
 	@Override
 	public void onClick(View v) {
-		//superController.performPositive(view.activity);		
+		try{
+			int numberOfGuests = Integer.parseInt(view.guestSelectionEnteringNumberField.getText().toString());
+			model.setNumberOfGuests(numberOfGuests);
+			SuperController.performPositive(view.activity, null);	
+		} catch (NumberFormatException e){
+			view.guestSelectionEnteringNumberField.setText("");
+		}
+			
 	}
 	
 }
